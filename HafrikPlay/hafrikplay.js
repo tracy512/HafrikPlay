78bbd91cc4f4127fc6d4fd027d6346f28eb3fc89
tracy512 <107678675+tracy512@users.noreply.github.com>
2023-12-04 21:04:17 +0000

    Add files via upload

---
+++ HafrikPlay/hafrikplay.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay:{
+      delay:3000,
+    },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
